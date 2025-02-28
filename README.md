4535bd1d21327b8a6ec4b3d403c813b81a5fc371
Błażej Niewiadomski <blaz.nie@protonmail.com>
2025-02-28 09:35:44 +0000

    update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# My Org Roam Notes
+# org roam notes
+This repo contains org files for my notes, hosted [here](https://implicit-image.github.io/notes).
---
+++ day1.rs
@@ -0,0 +1,9 @@
+use std::io;
+use std::fs;
+use std::vec::{Vec};
+
+fn main() {
+    let input = fs::read_to_string("~/Downloads/aoc/input1").expect("Failed to read a file");
+
+    let left_numbers, right_numbers = Vec::new();
+}
